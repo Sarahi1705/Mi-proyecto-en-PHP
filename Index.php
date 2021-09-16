1ea3255c6f88066da46a7f46a38ce6c922e1a6b6
Sarahi1705 <89057328+Sarahi1705@users.noreply.github.com>
2021-09-16 18:21:01 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ 
+    <title>Mi blog personal con Materialize</title>
+   
+   
+  </head>
+  <body>
+    <!--Encabezado-->
+  <?php include 'views/header.php' ?>
+    
+           
+  <?php include 'controller/routing.php' ?> 
+       
+
+     
+
+
+
+
+ <!--Pie de pagina-->
+ <?php include 'views/footer.php' ?>
+
+      
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
